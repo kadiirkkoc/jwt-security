cbf7704044244f51dd6d31ed3eb30d95051e2fe6
Kadir Koç <kadiirkkoc@gmail.com>
2023-07-31 16:39:36 +0200

    users not match between userdetails and user and added some comment in jwtservice

--- src/main/java/com/kadir/security/service/JwtService.java
+++ src/main/java/com/kadir/security/service/JwtService.java
@@ -27,19 +27,21 @@ public class JwtService {
         return generateToken(new HashMap<>(),userDetails);
     }
     public String generateToken(
-            Map<String , Object> extraClaims,
-            UserDetails userDetails
+            Map<String , Object> extraClaims,//A map of additional claims that you want to include in the generated JWT.
+                                             // These claims represent additional information related to the user or any custom data you want to associate with the token.
+            UserDetails userDetails //An instance of UserDetails, which represents the authenticated user's information, such as username, password, and authorities.
     ){
         return Jwts
-                .builder()
+                .builder()//Creates a JWT builder object to build the JWT.
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
+                .compact(); //Builds the final JWT and returns it as a compact string.
     }
 
+    //user info which logged in with mail and password in the token compare with*******
     public boolean isTokenValid(String token,UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
@@ -60,11 +62,11 @@ public class JwtService {
 
     private Claims extractAllCalims(String token){
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parserBuilder() //Creates a JWT parser object to process the JWT.
+                .setSigningKey(getSignInKey()) //Specifies the signing key used for JWT verification. The getSignInKey() method can be called to obtain this key. JWT verification ensures that the transported data is trustworthy and hasn't been altered.
+                .build() //Completes the configuration of the JWT parser object
+                .parseClaimsJws(token) //Splits the provided JWT into its components and extracts the data within it.
+                .getBody(); // Returns the "body" section (claims) of the JWT. The "body" of the JWT is a JSON object that carries user information and other authorities.
     }
 
     private Key getSignInKey() {
